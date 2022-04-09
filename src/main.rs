888dedb366589be7039044e30f1329862a585948
Sławomir Chrobak <slawomir.chrobak@schibsted.com>
2022-04-09 17:18:00 +0200

    feat: simple implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use minigrep::Config;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+
+    println!("Searching for {} in file {}", config.query, config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
